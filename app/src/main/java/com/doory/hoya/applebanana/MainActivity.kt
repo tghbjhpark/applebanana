a436d7c9e99577b4308c68d27c718837acd5498f
Jongho <tghbjhpark@gmail.com>
2021-05-21 17:34:12 +0900

    Initial commit

---
+++ app/src/main/java/com/doory/hoya/applebanana/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.doory.hoya.applebanana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doory.hoya.applebanana.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
